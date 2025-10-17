51738ed0f7100cf2195113a56b22e39c55c1c787
Plant-mobile <project2003b@gmail.com>
2025-10-17 18:38:15 +0300

    Termination of translation and message systems

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Slot } from 'expo-router'
 import { UserProvider } from '../services/auth/auth'
+import '../services/translateService'
+import Toast from 'react-native-toast-message'
+
 
 export default function RootLayout() {
   return (
     <UserProvider>
       <Slot />
+      <Toast />
     </UserProvider>
   )
 }
