1ba5d49deb90adc76109df955ad7d1fd38dde68f
ZumurudaNasayreh <zumuruda03nasayreh@gmail.com>
2025-11-08 14:13:54 +0300

    sol error i main

--- AI2/MyCode/main.py
+++ AI2/MyCode/main.py
@@ -1,6 +1,7 @@
 from predict import single_prediction
 
 if __name__ == "__main__":
-    single_prediction(r"C:\Users\user\Desktop\zmzm\AI2\test_images\blackrotapple.jpg")
-    single_prediction(r"C:\Users\user\Desktop\zmzm\AI2\test_images\tomato_late_blight.jpg")
-    single_prediction(r"C:\Users\user\Desktop\zmzm\AI2\test_images\tomato-mold.jpg")
+    single_prediction(r"C:\Users\user\Desktop\project\PlantWiseApp\AI2\test_images\background_without_leaves.jpg")
+    single_prediction(r"C:\Users\user\Desktop\project\PlantWiseApp\AI2\test_images\tomato_late_blight.JPG")
+    single_prediction(r"C:\Users\user\Desktop\project\PlantWiseApp\AI2\test_images\tomato-mold.jpg")
+
--- AI2/MyCode/model_loader.py
+++ AI2/MyCode/model_loader.py
@@ -1,9 +1,12 @@
 import torch
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 from trainModel import CNN
 from splitData import targets_size, model_path, device
 
 def load_trained_model():
     model = CNN(targets_size)
+    
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
     model.eval()
